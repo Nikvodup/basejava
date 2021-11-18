172a093efc5b6bfef49e49e8fa2747938d3f89f0
Alex <ransbach25@gmail.com>
2021-11-18 23:02:05 +0500

    basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
 /**
@@ -7,20 +8,22 @@ import java.util.function.Predicate;
  */
 public class ArrayStorage {
 
+    private static AtomicInteger size= new AtomicInteger(0);
+    Resume[] storage = new Resume[10000];
     //Predicate<Resume> condition = b->b!=null;
     Predicate<Resume> condition = Objects::nonNull;
 
-    Resume[] storage = new Resume[10000];
 
     void clear() {
-        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
+        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);//storage2 is a copy of storage but without nulls.
         Arrays.fill(storage, 0, storage2.length + 1, null);
+        size.set(0) ;
     }
 
     void save(Resume r) {
-
         Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
         storage[storage2.length] = r;
+         size.getAndIncrement();
     }
 
     Resume get(String uuid) {
@@ -36,27 +39,27 @@ public class ArrayStorage {
         Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
         for (Resume r : storage2) {
             if (uuid.equals(r.toString())) {
-    int index = Arrays.asList(storage2).indexOf(r);
-    storage[index] = null;
-    for (int i = index + 1; i < storage.length; i++) {// really moves null(after deleting a resume) behind String objects
-        storage[i - 1] = storage[i];
-        storage[i] = null;
-         }
-       }
-     }
-   }
-
+                int index = Arrays.asList(storage2).indexOf(r);
+                storage[index] = null;
+                for (int i = index + 1; i < storage.length; i++) {// really moves null(after deleting a resume) behind String objects
+                    storage[i - 1] = storage[i];
+                    storage[i] = null;
+                }
+            }
+        }
+        size.getAndDecrement();
+    }
 
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-            return Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
+        return Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
     }
 
+
     int size() {
-        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
-        return storage2.length;
+        return size.get();
     }
 }
