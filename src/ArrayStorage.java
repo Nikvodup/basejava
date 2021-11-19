7d9c2c16f5ac2c4180362b1b70be8cb311df6b6c
Alex <ransbach25@gmail.com>
2021-11-20 02:39:49 +0500

    basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,44 +1,46 @@
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
 
-    private static AtomicInteger size= new AtomicInteger(0);
+    private static AtomicInteger size = new AtomicInteger();
     Resume[] storage = new Resume[10000];
-    //Predicate<Resume> condition = b->b!=null;
-    Predicate<Resume> condition = Objects::nonNull;
+   // Resume r = new Resume();
 
 
     void clear() {
-        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);//storage2 is a copy of storage but without nulls.
-        Arrays.fill(storage, 0, storage2.length + 1, null);
-        size.set(0) ;
+        Arrays.fill(storage, 0, size() + 1, null);
+        size.set(0);
     }
+
 
     void save(Resume r) {
-        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
-        storage[storage2.length] = r;
-         size.getAndIncrement();
+        storage[size()] = r;
+        size.getAndIncrement();
     }
 
+
     Resume get(String uuid) {
-        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
-        for (Resume r : storage2) {
-            if (r.uuid.equals(uuid))
-                return r;
+        //   return  Arrays.stream(storage).filter(r-> r.uuid.equals(uuid)).findAny().orElse(null);
+        for (Resume r : storage) {
+            if (r != null) {
+                if (uuid.equals(r.uuid)) {
+                    int index = Arrays.asList(storage).indexOf(r);
+                    return storage[index];
+                }
+            }
         }
         return null;
     }
 
+
     void delete(String uuid) {
-        Resume[] storage2 = Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
+        Resume[] storage2 = Arrays.stream(storage).limit(size()).toArray(Resume[]::new);
         for (Resume r : storage2) {
-            if (uuid.equals(r.toString())) {
+            if (uuid.equals(r.uuid)) {
                 int index = Arrays.asList(storage2).indexOf(r);
                 storage[index] = null;
                 for (int i = index + 1; i < storage.length; i++) {// really moves null(after deleting a resume) behind String objects
@@ -55,7 +57,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.stream(storage).filter(condition).toArray(Resume[]::new);
+        return Arrays.stream(storage).limit(size()).toArray(Resume[]::new);
     }
 
 
