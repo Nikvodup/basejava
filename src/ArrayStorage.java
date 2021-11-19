fee93a4a7c25d87985cee9cc1db4b55aab887cf7
Alex <ransbach25@gmail.com>
2021-11-20 03:20:35 +0500

    basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,7 +8,6 @@ public class ArrayStorage {
 
     private static AtomicInteger size = new AtomicInteger();
     Resume[] storage = new Resume[10000];
-   // Resume r = new Resume();
 
 
     void clear() {
@@ -24,6 +23,7 @@ public class ArrayStorage {
 
 
     Resume get(String uuid) {
+        // Resume r = new Resume();
         //   return  Arrays.stream(storage).filter(r-> r.uuid.equals(uuid)).findAny().orElse(null);
         for (Resume r : storage) {
             if (r != null) {
@@ -38,10 +38,9 @@ public class ArrayStorage {
 
 
     void delete(String uuid) {
-        Resume[] storage2 = Arrays.stream(storage).limit(size()).toArray(Resume[]::new);
-        for (Resume r : storage2) {
-            if (uuid.equals(r.uuid)) {
-                int index = Arrays.asList(storage2).indexOf(r);
+        for (Resume r : storage) {
+            if (r != null && uuid.equals(r.uuid)) {
+                int index = Arrays.asList(storage).indexOf(r);
                 storage[index] = null;
                 for (int i = index + 1; i < storage.length; i++) {// really moves null(after deleting a resume) behind String objects
                     storage[i - 1] = storage[i];
