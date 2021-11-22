b323646d0baa49f7f941f28f3745d75ebae8381d
Alex <ransbach25@gmail.com>
2021-11-22 15:59:43 +0500

    basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,63 +1,49 @@
 import java.util.Arrays;
+import java.util.Objects;
 
-/**
- * Array based storage for Resumes
- */
+
 public class ArrayStorage {
 
     private static int size;
     Resume[] storage = new Resume[10000];
 
-
     void clear() {
-        Arrays.fill(storage, 0, size + 1, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-
     void save(Resume r) {
-            storage[size] = r;
-            size++;
-
+        storage[size] = r;
+        size++;
     }
-
 
     Resume get(String uuid) {
         return Arrays.stream(storage).limit(size).filter(r -> r.uuid.equals(uuid)).findAny().orElse(null);
-
-     /*   for (Resume r : storage){
-            if(r!=null && r.uuid==uuid){
-                return r;
-            }
-        }
-        return null;  */
     }
-
 
     void delete(String uuid) {
         Resume resumeToBeDeleted = Arrays.stream(storage).limit(size).filter(r -> r.uuid.equals(uuid)).findAny().orElse(null);
         Resume[] storage2;
-        for (int i = 0; i < storage.length - 1 ; i++) {
+        for (int i = 0; i < storage.length - 1; i++) {
             if (storage[i] == resumeToBeDeleted) {
                 storage2 = new Resume[storage.length - 1];
                 System.arraycopy(storage, 0, storage2, 0, i);
-                if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, storage2, i, storage.length - 1 - i);
+                if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, storage2, i, size - 1 - i);
                 storage = storage2;
+                size--;
                 break;
-            }
-        }
-        size--;
-    }
+            }  else if(uuid==null) {return;}
+         }
+     }
+
 
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-     //   return Arrays.stream(storage).limit(size).toArray(Resume[]::new);
-        return Arrays.copyOf(storage,size);
+        return Arrays.copyOf(storage, size);
     }
-
 
     int size() {
         return size;
--- src/Resume.java
+++ src/Resume.java
@@ -1,16 +1,65 @@
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
 /**
  * Initial resume class
  */
-public class Resume {
+public class Resume implements Comparable, Comparator<Resume> {
 
     // Unique identifier
-    String uuid;
+    String uuid ;
 
     @Override
     public String toString() {
-
         return uuid;
     }
 
 
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int compare(Resume o1, Resume o2) {
+        return 0;
+    }
+
+    @Override
+    public Comparator<Resume> reversed() {
+        return null;
+    }
+
+    @Override
+    public Comparator<Resume> thenComparing(Comparator<? super Resume> other) {
+        return null;
+    }
+
+    @Override
+    public <U> Comparator<Resume> thenComparing(Function<? super Resume, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        return null;
+    }
+
+    @Override
+    public <U extends Comparable<? super U>> Comparator<Resume> thenComparing(Function<? super Resume, ? extends U> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Resume> thenComparingInt(ToIntFunction<? super Resume> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Resume> thenComparingLong(ToLongFunction<? super Resume> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Resume> thenComparingDouble(ToDoubleFunction<? super Resume> keyExtractor) {
+        return null;
+    }
 }
