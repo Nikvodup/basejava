ba2645d69e383543b5ed17b9a37a32d4d311fdee
Alex <ransbach25@gmail.com>
2021-11-23 14:46:06 +0500

    basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -18,24 +18,26 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        return Arrays.stream(storage).limit(size).filter(r -> r.uuid.equals(uuid)).findAny().orElse(null);
+        return Arrays.stream(storage).limit(size).filter(Objects::nonNull).filter(r -> r.uuid.equals(uuid)).findAny().orElse(null);
     }
 
     void delete(String uuid) {
-        Resume resumeToBeDeleted = Arrays.stream(storage).limit(size).filter(r -> r.uuid.equals(uuid)).findAny().orElse(null);
-        Resume[] storage2;
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == resumeToBeDeleted) {
-                storage2 = new Resume[storage.length - 1];
-                System.arraycopy(storage, 0, storage2, 0, i);
-                if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, storage2, i, size - 1 - i);
-                storage = storage2;
-                size--;
-                break;
-            }  else if(uuid==null) {return;}
-         }
-     }
 
+       for(Resume r : storage){
+            if (r!=null && uuid==r.uuid){
+                for (int i=0;i<size;i++){
+                    if (storage[i]==r) {
+                        Resume[] storage2 = new Resume[storage.length-1];
+                        System.arraycopy(storage, 0, storage2, 0, i);
+                        if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, storage2, i, storage.length - 1 - i);
+                        storage = storage2;
+                        size--;
+                        break;
+                    }else if (uuid == null || uuid!=r.uuid){return;}
+                }
+            }
+        }
+    }
 
 
     /**
