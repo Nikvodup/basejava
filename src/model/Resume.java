92e40dbf554dc13d05ad9e0fc4d597896bc0bdae
Alex <ransbach25@gmail.com>
2022-05-26 22:07:02 +0500

    basejava

--- src/model/Resume.java
+++ src/model/Resume.java
@@ -1,20 +1,11 @@
 package model;
 
-/**
- * Initial resume class
- */
-public class Resume  {
+public class Resume implements Comparable<Resume>{
+
     // Unique identifier
-   private String uuid;
+    private String uuid;
 
-
-
-    @Override
-    public String toString() {
-        return uuid;
-    }
-
-    public String getUuid(){
+    public String getUuid() {
         return uuid;
     }
 
@@ -22,4 +13,28 @@ public class Resume  {
         this.uuid = uuid;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
+    }
 }
